e637b9f5a5417900d867deb4f0347765cf188720
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2025-06-04 02:27:36 -0500

    add app.dart

---
+++ lib/app.dart
@@ -0,0 +1,115 @@
+// lib/app.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'core/themes/app_theme.dart';
+import 'core/routes/app_router.dart';
+import 'core/routes/route_names.dart';
+import 'providers/auth_provider.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'NutriApp - Pacientes',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: AppColors.mainOrange,
+            primary: AppColors.mainOrange,
+            secondary: AppColors.mediumOrange,
+          ),
+          fontFamily: 'Montserrat',
+          appBarTheme: const AppBarTheme(
+            backgroundColor: AppColors.mainOrange,
+            foregroundColor: Colors.white,
+            elevation: 0,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.mainOrange,
+              foregroundColor: Colors.white,
+              elevation: 2,
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              textStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor: AppColors.mainOrange,
+              textStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.grey[300]!),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.grey[300]!),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(
+                color: AppColors.mainOrange,
+                width: 2,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Colors.red),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Colors.red, width: 2),
+            ),
+            labelStyle: TextStyle(color: Colors.grey[700]),
+            hintStyle: TextStyle(color: Colors.grey[500]),
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 16,
+            ),
+          ),
+          cardTheme: CardTheme(
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            color: Colors.white,
+          ),
+          scaffoldBackgroundColor: AppColors.whiteBackground,
+          iconTheme: const IconThemeData(color: AppColors.mainOrange),
+          dividerTheme: DividerThemeData(
+            color: Colors.grey[300],
+            thickness: 1,
+            space: 1,
+          ),
+        ),
+        initialRoute: RouteNames.login,
+        onGenerateRoute: AppRouter.generateRoute,
+      ),
+    );
+  }
+}
