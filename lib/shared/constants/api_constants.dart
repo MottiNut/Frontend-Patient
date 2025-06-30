ca13a0e90639a67ec40b9446415f1b22670c8b7b
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2025-06-29 19:44:16 -0500

    update url

--- lib/shared/constants/api_constants.dart
+++ lib/shared/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:5000/api/bff';
+  static const String baseUrl = 'https://motti-backkend-2.onrender.com/api/bff';
 
   static const String auth = '$baseUrl/auth';
   static const String nutrition = '$baseUrl/patient/nutrition-plans';
